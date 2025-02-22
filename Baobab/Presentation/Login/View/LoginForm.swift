6acc25ef537e5d939394cc23a256669d069ea126
jeungHunLee <ksjs1111@gmail.com>
2025-02-22 23:44:09 +0900

    BV2-134 (FAET): NavigationStack 추가

--- Baobab/Presentation/Login/View/LoginForm.swift
+++ Baobab/Presentation/Login/View/LoginForm.swift
@@ -19,66 +19,74 @@ struct LoginForm: View {
     }
     
     var body: some View {
-        ScrollView(.vertical) {
-            VStack(spacing: 20) {
-                LogoView()
-                    .padding(.bottom, 20)
-                
-                BorderedTextField(input: $viewModel.email,
-                                  placeholder: "이메일을 입력하세요.",
-                                  isSecureText: false)
-                
-                BorderedTextField(input: $viewModel.password,
-                                  placeholder: "비밀번호를 입력하세요.",
-                                  isSecureText: true)
-                
-                AutoLoginBtn(isAutoLogin: $viewModel.isAutoLogin)
-                
-                Button {
+        NavigationStack {
+            ScrollView(.vertical) {
+                VStack(spacing: 20) {
+                    LogoView()
+                        .padding(.bottom, 20)
                     
-                } label: {
-                    Text("로그인")
-                        .bold()
-                        .foregroundStyle(.white)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(.accent)
-                }
-                .cornerRadius(30)
-                .padding(.top)
-                
-                SectionSeparator()
+                    BorderedTextField(input: $viewModel.email,
+                                      placeholder: "이메일을 입력하세요.",
+                                      isSecureText: false)
+                    
+                    BorderedTextField(input: $viewModel.password,
+                                      placeholder: "비밀번호를 입력하세요.",
+                                      isSecureText: true)
+                    
+                    AutoLoginBtn(isAutoLogin: $viewModel.isAutoLogin)
+                    
+                    Button {
+                        viewModel.login()
+                    } label: {
+                        Text("로그인")
+                            .bold()
+                            .foregroundStyle(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(.accent)
+                    }
+                    .cornerRadius(30)
                     .padding(.top)
-                
-                Button {
-                    isShowingSignupForm.toggle()
-                } label: {
-                    Text("회원가입")
-                        .bold()
-                        .font(.subheadline)
-                        .foregroundStyle(.gray)
+                    
+                    SectionSeparator()
+                        .padding(.top)
+                    
+                    Button {
+                        isShowingSignupForm.toggle()
+                    } label: {
+                        Text("회원가입")
+                            .bold()
+                            .font(.subheadline)
+                            .foregroundStyle(.gray)
+                    }
+                    
+                    Spacer()
                 }
-                
-                Spacer()
+                .padding([.leading, .trailing, .bottom])
+                .padding(.top, UIScreen.main.bounds.width * 0.2)
             }
-            .padding([.leading, .trailing, .bottom])
-            .padding(.top, UIScreen.main.bounds.width * 0.2)
-        }
-        .scrollDisabled(!isKeyboardActive)
-        .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { _ in
-            isKeyboardActive = true
-        }
-        .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)) { _ in
-            isKeyboardActive = false
-        }
-        .toolbar(.hidden)
-        .fullScreenCover(isPresented: $isShowingSignupForm) {
-            NavigationStack {
-                SignupForm(viewModel: SignupFormViewModel())
+            .scrollDisabled(!isKeyboardActive)
+            .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { _ in
+                isKeyboardActive = true
             }
-        }
-        .onDisappear {
-            viewModel.loginTask?.cancel()
+            .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)) { _ in
+                isKeyboardActive = false
+            }
+            .toolbar(.hidden)
+            .fullScreenCover(isPresented: $isShowingSignupForm) {
+                NavigationStack {
+                    SignupForm(viewModel: SignupFormViewModel())
+                }
+            }
+            .onDisappear {
+                viewModel.loginTask?.cancel()
+            }
+            .navigationDestination(isPresented: $viewModel.isLoginComplete) {
+                EmptyView()
+            }
+            .alert(viewModel.alertMessage, isPresented: $viewModel.isShowingAlert) {
+                Button("확인") {}
+            }
         }
     }
 }
@@ -170,6 +178,6 @@ fileprivate struct SectionSeparator: View {
 
 #Preview {
     NavigationStack {
-        LoginForm(viewModel: Container.shared.loginViewModel())
+        LoginForm(viewModel: LoginFormModel())
     }
 }
