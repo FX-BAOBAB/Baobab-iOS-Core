4fb331c4efb72e643ac2bc062412dcec49f5ca5e
jeungHunLee <ksjs1111@gmail.com>
2025-03-10 11:08:22 +0900

    BV2-186 (TEST): TradeArticleTableViewModel 유닛 테스트 추가

---
+++ BaobabTests/TradeArticleTests/TradeArticleTableViewModelTest.swift
@@ -0,0 +1,56 @@
+//
+//  TradeArticleTableViewModelTest.swift
+//  BaobabTests
+//
+//  Created by 이정훈 on 3/10/25.
+//
+
+import Alamofire
+import Factory
+import RxSwift
+import XCTest
+@testable import Baobab
+
+final class TradeArticleTableViewModelTest: XCTestCase {
+    private var interceptor: TokenInterceptor!
+    private var viewModel: TradeArticleTableViewModel!
+    private var disposeBag: DisposeBag!
+
+    @MainActor
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [MockTradeArticleListURLProtocol.self]
+        let session = Session(configuration: configuration)
+        Container.shared.remoteDataSource.register {
+            let dataSource = RemoteDataSource(session: session)
+            self.interceptor = TokenInterceptor(remoteDataSource: dataSource)
+            dataSource.tokenInterceptor = self.interceptor
+            return dataSource
+        }
+        viewModel = TradeArticleTableViewModel()
+        disposeBag = DisposeBag()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        interceptor = nil
+        viewModel = nil
+        disposeBag = nil
+    }
+
+    @MainActor
+    func test_viewModel에서_게시글_요청하면_게시글_리스트_반환() throws {
+        let expectation = XCTestExpectation(description: "fetch article list")
+        viewModel.articles
+            .skip(1)
+            .subscribe(onNext: {
+                XCTAssertEqual($0.count, 3)
+                expectation.fulfill()
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.fetchArticles()
+        wait(for: [expectation], timeout: 1)
+    }
+}
