8af86927dcdb44335849073067ca6b1ac097e1ac
jeungHunLee <ksjs1111@gmail.com>
2025-04-01 17:53:29 +0900

    BV2-202 (FEAT): firebase-ios-sdk 설정

--- Baobab/Application/AppDelegate.swift
+++ Baobab/Application/AppDelegate.swift
@@ -6,17 +6,44 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        setupFirebaseMessaging(with: application)
+        
         return true
     }
-
+    
+    private func setupFirebaseMessaging(with application: UIApplication) {
+        let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
+        
+        if let filePath, let fileOptions = FirebaseOptions(contentsOfFile: filePath) {
+            FirebaseApp.configure(options: fileOptions)
+        }
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]    //필요한 알림 권한
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        application.registerForRemoteNotifications()    //Apple Push Notification service 등록
+        
+        Messaging.messaging().delegate = self
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken    //device token을 Firebase messaging에 등록
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -30,7 +57,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    // MARK: - foreground에서 알림 수신
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner])
+    }
+    
+    // MARK: - background에서 알림 수신
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+}
 
-
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+        print(fcmToken)
+    }
 }
 
