f6f8486411dd6088058d22691f57272a2a77b7cb
jeungHunLee <ksjs1111@gmail.com>
2025-04-08 09:00:50 +0900

    BV2-210 (TEST): ChatRoomTableViewModel 테스트 코드 추가

--- BaobabTests/ChatRoomListTest/FetchChatRoomsTest.swift
+++ BaobabTests/ChatRoomListTest/FetchChatRoomsTest.swift
@@ -7,11 +7,14 @@
 
 import Alamofire
 import Factory
+import RxSwift
 import XCTest
 @testable import Baobab
 
 final class FetchChatRoomsTest: XCTestCase {
     private var remoteDataSource: RemoteDataSourceProtocol!
+    private var viewModel: ChatRoomTableViewModel!
+    private var disposeBag: DisposeBag!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -24,12 +27,16 @@ final class FetchChatRoomsTest: XCTestCase {
         }
         
         remoteDataSource = Container.shared.remoteDataSource()
+        viewModel = ChatRoomTableViewModel()
+        disposeBag = DisposeBag()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         
         remoteDataSource = nil
+        viewModel = nil
+        disposeBag = nil
     }
     
     func test_사용자_채팅목록_조회_성공() {
@@ -49,6 +56,20 @@ final class FetchChatRoomsTest: XCTestCase {
             }
         }
         
+        wait(for: [expectation], timeout: 2)
+    }
+    
+    func test_viewModel에서_채팅목록_조회_성공() {
+        let expectation = XCTestExpectation(description: "fetch chat rooms in viewModel")
+        viewModel.chatRooms
+            .skip(1)    //초기값 생략
+            .subscribe(onNext: {
+                XCTAssertTrue(!$0.isEmpty)
+                expectation.fulfill()
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.fetchChatRooms()
         wait(for: [expectation], timeout: 2)
     }
 
